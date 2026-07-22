b289b000336f56f29407aa858b27a01340e7ae25
arsen <agevorgan025@gmail.com>
2026-07-22 12:11:39 +0300

    ф

--- sway/status.sh
+++ sway/status.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 while true; do
+    DATE_TIME=$(date +'%d.%m.%y %H:%M')
     BAT=$(cat /sys/class/power_supply/BAT1/capacity 2>/dev/null)
     BAT_STAT=$(cat /sys/class/power_supply/BAT1/status 2>/dev/null)
-    BRT=$(brightnessctl i | grep -oP '\(\K\d+(?=%\))')
-    DATE_TIME=$(date +'%d.%m.%y %H:%M')
-    VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d", $2 * 100}')
-    MUTE=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -c MUTED)
+    BNOW=$(cat /sys/class/backlight/intel_backlight/brightness)
+    BMAX=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+    BRT=$(( BNOW * 100 / BMAX ))
+    WP_OUT=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+    VOL=$(echo "$WP_OUT" | awk '{printf "%d", $2 * 100}')
+    MUTE=$(echo "$WP_OUT" | grep -c MUTED)
+    MEM=$(free | awk '/Mem:/ {printf "%.2f GiB/%.2f GiB\n", $3/1024/1024, $2/1024/1024}')
+    ROUTE_DEV=$(ip route get 1.1.1.1 2>/dev/null | awk '{print $5; exit}')
+    IP_ADDR=$(ip -o -4 addr show dev "$ROUTE_DEV" 2>/dev/null | awk '{split($4,a,"/"); print a[1]}')
+    WIFI=$(iwctl station wlan0 show | awk '/Connected network/ {print $3}')
 
     if [ "$BAT_STAT" = "Charging" ]; then
         BAT_ICON="󰁹"
@@ -14,15 +21,27 @@ while true; do
         BAT_ICON="󰂎"
     fi
 
+    BRT_ICON=" "
+
     if [ "$MUTE" -gt 0 ]; then
         VOL_ICON=" "
     else
         VOL_ICON=" "
     fi
 
-    BRT_ICON=" "
+    MEM_ICON=" "
 
-    echo "$VOL% $VOL_ICON | $BRT% $BRT_ICON | $BAT% $BAT_ICON | $DATE_TIME"
+    if [ -z "$WIFI" ]; then
+        WIFI_ICON=" "
+        WIFI="Disconnected"
+    else
+        WIFI_ICON=" "
+        if [ -n "$IP_ADDR" ]; then
+            WIFI="$WIFI ($IP_ADDR)"
+        fi
+    fi
+
+    echo "$WIFI_ICON $WIFI | $MEM_ICON $MEM | $VOL_ICON $VOL% | $BRT_ICON $BRT% | $BAT_ICON $BAT% | $DATE_TIME"
 
     sleep 1
 done
